55effd61b8714b41241954e0232fdc398dfab0d1
Claudiney Mendes da Silva <claudineymendez@gmail.com>
2026-03-06 19:58:14 -0300

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,11 @@
+
+public class ForEach {
+    public static void main (String[] args){
+        String[] frutas = {"Banana", "Laranja", "Maça", "Pera", "Uva"};
+
+        for (String fruta : frutas) {
+            System.out.println("A fruta é: " + fruta);
+
+        }
+    }
+}
